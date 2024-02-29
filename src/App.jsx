6580616b24cc3a3699a209276a338129c55d20ac
svivek19 <svivek030503@gmail.com>
2024-02-29 20:21:13 +0530

    firebase setuop finish

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useEffect } from "react";
 import FormInput from "./component/forms/FormInput";
 import FormTextArea from "./component/forms/FormTextArea";
 import { useForm } from "react-hook-form";
 import FormSelect from "./component/forms/FormSelect";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { z } from "zod";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "./firebase/index";
 
 const formSchema = z.object({
   jobRole: z.string(),
@@ -27,6 +29,21 @@ const App = () => {
   const sendThisToServer = (data) => {
     console.log(data);
   };
+
+  useEffect(() => {
+    async function getDataFromFirebase() {
+      const querySnapshot = await getDocs(collection(db, "candidates"));
+      querySnapshot.forEach((doc) => {
+        console.log(`${doc.id} => ${doc.data()}`);
+      });
+
+      if (querySnapshot.docs.length === 0) {
+        console.log("no records!");
+      }
+    }
+
+    getDataFromFirebase();
+  }, []);
 
   return (
     <div className="bg-gray-100 min-h-screen">
@@ -77,6 +94,65 @@ const App = () => {
               Submit
             </button>
           </form>
+        </section>
+
+        {/* Display section */}
+        <section className="my-10">
+          <div className="relative overflow-x-auto">
+            <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+              <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                <tr>
+                  <th scope="col" className="px-6 py-3">
+                    Product name
+                  </th>
+                  <th scope="col" className="px-6 py-3">
+                    Color
+                  </th>
+                  <th scope="col" className="px-6 py-3">
+                    Category
+                  </th>
+                  <th scope="col" className="px-6 py-3">
+                    Price
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                  <th
+                    scope="row"
+                    className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                  >
+                    Apple MacBook Pro 17"
+                  </th>
+                  <td className="px-6 py-4">Silver</td>
+                  <td className="px-6 py-4">Laptop</td>
+                  <td className="px-6 py-4">$2999</td>
+                </tr>
+                <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                  <th
+                    scope="row"
+                    className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                  >
+                    Microsoft Surface Pro
+                  </th>
+                  <td className="px-6 py-4">White</td>
+                  <td className="px-6 py-4">Laptop PC</td>
+                  <td className="px-6 py-4">$1999</td>
+                </tr>
+                <tr className="bg-white dark:bg-gray-800">
+                  <th
+                    scope="row"
+                    className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                  >
+                    Magic Mouse 2
+                  </th>
+                  <td className="px-6 py-4">Black</td>
+                  <td className="px-6 py-4">Accessories</td>
+                  <td className="px-6 py-4">$99</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
         </section>
       </main>
     </div>
