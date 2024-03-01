940b53ae02e2fbbc2e67a3be7fd2dc94df7204d7
svivek19 <svivek030503@gmail.com>
2024-03-01 10:26:26 +0530

    env addded

--- src/App.jsx
+++ src/App.jsx
@@ -111,7 +111,7 @@ const App = () => {
 
         {/* Display section */}
         <section className="my-10">
-          <div className="relative overflow-x-auto">
+          <div className="relative overflow-x-auto rounded">
             <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
               <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                 <tr>
